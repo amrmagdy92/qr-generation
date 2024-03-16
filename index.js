d4e3d5a655ae891d8a3dd9f76b751bda9b9dba4f
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2024-03-17 01:25:05 +0200

    Removed all TODOs and commented functions, and added default qrOptions object

--- index.js
+++ index.js
@@ -5,94 +5,87 @@ if (isDarkMode()) {
     document.querySelector("html").setAttribute("data-bs-theme", window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
 }
 
-// TODO: remove gradient color support for now
-// TODO: check how to implement idleState
-// TODO: check how to send only updates instead of the whole object
-// TODO: remove the qrOptions variable as it's no longer needed
-
 let qrOptions = {
-    options: {
-        "width": 300,
-        "height": 300,
-        "data": "https://www.linkedin.com/in/amaa/",
-        "margin": 0,
-        "qrOptions": {
-            "typeNumber": "0",
-            "mode": "Byte",
-            "errorCorrectionLevel": "H"
+    "width": 300,
+    "height": 300,
+    "data": "https://www.linkedin.com/in/amaa/",
+    "margin": 0,
+    "qrOptions": {
+        "typeNumber": "0",
+        "mode": "Byte",
+        "errorCorrectionLevel": "H"
+    },
+    "imageOptions": {
+        "hideBackgroundDots": true,
+        "imageSize": 0.2,
+        "margin": 0
+    },
+    "dotsOptions": {
+        "type": "dots",
+        "color": "#0a66c2"
+    },
+    "backgroundOptions": {
+        "color": "#ffffff"
+    },
+    "image": "",
+    "dotsOptionsHelper": {
+        "colorType": {
+            "single": true,
+            "gradient": false
         },
-        "imageOptions": {
-            "hideBackgroundDots": true,
-            "imageSize": 0.2,
-            "margin": 0
+        "gradient": {
+            "linear": true,
+            "radial": false,
+            "color1": "#6a1a4c",
+            "color2": "#6a1a4c",
+            "rotation": "0"
+        }
+    },
+    "cornersSquareOptions": {
+        "type": "dot",
+        "color": "#0a66c2"
+    },
+    "cornersSquareOptionsHelper": {
+        "colorType": {
+            "single": true,
+            "gradient": false
         },
-        "dotsOptions": {
-            "type": "dots",
-            "color": "#0a66c2"
+        "gradient": {
+            "linear": true,
+            "radial": false,
+            "color1": "#000000",
+            "color2": "#000000",
+            "rotation": "0"
+        }
+    },
+    "cornersDotOptions": {
+        "type": "",
+        "color": "#000000"
+    },
+    "cornersDotOptionsHelper": {
+        "colorType": {
+            "single": true,
+            "gradient": false
         },
-        "backgroundOptions": {
-            "color": "#ffffff"
+        "gradient": {
+            "linear": true,
+            "radial": false,
+            "color1": "#000000",
+            "color2": "#000000",
+            "rotation": "0"
+        }
+    },
+    "backgroundOptionsHelper": {
+        "colorType": {
+            "single": true,
+            "gradient": false
         },
-        "image": "",
-        "dotsOptionsHelper": {
-            "colorType": {
-                "single": true,
-                "gradient": false
-            },
-            "gradient": {
-                "linear": true,
-                "radial": false,
-                "color1": "#6a1a4c",
-                "color2": "#6a1a4c",
-                "rotation": "0"
-            }
-        },
-        "cornersSquareOptions": {
-            "type": "dot",
-            "color": "#0a66c2"
-        },
-        "cornersSquareOptionsHelper": {
-            "colorType": {
-                "single": true,
-                "gradient": false
-            },
-            "gradient": {
-                "linear": true,
-                "radial": false,
-                "color1": "#000000",
-                "color2": "#000000",
-                "rotation": "0"
-            }
-        },
-        "cornersDotOptions": {
-            "type": "",
-            "color": "#000000"
-        },
-        "cornersDotOptionsHelper": {
-            "colorType": {
-                "single": true,
-                "gradient": false
-            },
-            "gradient": {
-                "linear": true,
-                "radial": false,
-                "color1": "#000000",
-                "color2": "#000000",
-                "rotation": "0"
-            }
-        },
-        "backgroundOptionsHelper": {
-            "colorType": {
-                "single": true,
-                "gradient": false
-            },
-            "gradient": {
-                "linear": true,
-                "radial": false,
-                "color1": "#ffffff",
-                "color2": "#ffffff",
-                "rotation": "0"
-            }
+        "gradient": {
+            "linear": true,
+            "radial": false,
+            "color1": "#ffffff",
+            "color2": "#ffffff",
+            "rotation": "0"
         }
     }
 }
@@ -105,57 +98,10 @@ function validateFileSize(file) {
     }
 }
 
-// function mainOptionsFactory(width, height, data, margin, image) {
-//     qrOptions.options.width = width
-//     qrOptions.options.height = height
-//     qrOptions.options.data = data
-//     qrOptions.options.margin = margin
-//     qrOptions.options.image = image
-// }
-
-// function dotsOptionsFactory(dotType, dotColor) {
-//     qrOptions.options.dotsOptions = {
-//         color: dotColor,
-//         type: dotType
-//     }
-// }
-
-// function cornerSquareOptionsFactory(cornerSquareType, cornerSquareColor) {
-//     qrOptions.options.cornersSquareOptions = {
-//         color: cornerSquareColor,
-//         type: cornerSquareType
-//     }
-// }
-
-// function cornerDotsOptionsFactory(style, color) {
-//     qrOptions.options.cornersDotOptions = {
-//         type: style,
-//         color: color
-//     }
-// }
-
-// function backgroundOptionsFactory() {}
-
-// function imageOptionsFactory(hideDots, imgSize, imgMargin) {
-//     qrOptions.options.imageOptions = {
-//         hideBackgroundDots: hideDots,
-//         imageSize: imgSize,
-//         margin: imgMargin
-//     }
-// }
-
-// function advQROptionsFactory(type, mode, correction) {
-//     qrOptions.options.qrOptions = {
-//         "typeNumber": type,
-//         "mode": mode,
-//         "errorCorrectionLevel": correction
-//     }
-// }
-
 function qrOptionsFactory(id) {
     return new Promise((resolve, reject) => {
         let element = document.getElementById(id)
-        if (Object.keys(qrOptions.options).includes(element.id)) {
+        if (Object.keys(qrOptions).includes(element.id)) {
             if (element.id === "image") {
                 let file = document.getElementById(id).files[0]
                 if (validateFileSize(file)) {
@@ -172,43 +118,43 @@ function qrOptionsFactory(id) {
                     element.value = null
                 }
             } else if (element.id === "dots-style") {
-                qrOptions.options.dotsOptions.type = element.value
+                qrOptions.dotsOptions.type = element.value
                 resolve(JSON.stringify(qrOptions))
             } else if (element.id === "corner-square-style") {
-                qrOptions.options.cornersSquareOptions.type = element.value
+                qrOptions.cornersSquareOptions.type = element.value
                 resolve(JSON.stringify(qrOptions))
             } else if (element.id === "corner-dots-style") {
-                qrOptions.options.cornersDotOptions.type = element.value
+                qrOptions.cornersDotOptions.type = element.value
                 resolve(JSON.stringify(qrOptions))
             } else if (element.id === "mode") {
-                qrOptions.options.qrOptions.mode = element.value
+                qrOptions.qrOptions.mode = element.value
                 resolve(JSON.stringify(qrOptions))
             } else if (element.id === "correction-level") {
-                qrOptions.options.qrOptions.errorCorrectionLevel = element.value
+                qrOptions.qrOptions.errorCorrectionLevel = element.value
                 resolve(JSON.stringify(qrOptions))
             } else if (element.id === "data") {
-                qrOptions.options.data = element.value
+                qrOptions.data = element.value
                 resolve(JSON.stringify(qrOptions))
             } else if (element.id === "height") {
-                qrOptions.options.height = element.value
+                qrOptions.height = element.value
                 resolve(JSON.stringify(qrOptions))
             } else if (element.id === "width") {
-                qrOptions.options.width = element.value
+                qrOptions.width = element.value
                 resolve(JSON.stringify(qrOptions))
             } else if (element.id === "margin") {
-                qrOptions.options.margin = element.value
+                qrOptions.margin = element.value
                 resolve(JSON.stringify(qrOptions))
             } else if (element.id === "image-size") {
-                qrOptions.options.imageOptions.imageSize = element.value
+                qrOptions.imageOptions.imageSize = element.value
                 resolve(JSON.stringify(qrOptions))
             } else if (element.id === "image-margin") {
-                qrOptions.options.imageOptions.margin = element.value
+                qrOptions.imageOptions.margin = element.value
                 resolve(JSON.stringify(qrOptions))
             } else if (element.id === "qr-type") {
-                qrOptions.options.qrOptions.typeNumber = element.value
+                qrOptions.qrOptions.typeNumber = element.value
                 resolve(JSON.stringify(qrOptions))
             } else if (element.id === "hide-background") {
-                qrOptions.options.imageOptions.hideBackgroundDots = element.checked
+                qrOptions.imageOptions.hideBackgroundDots = element.checked
                 resolve(JSON.stringify(qrOptions))
             }
         }
